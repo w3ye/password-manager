75dd67ff9f91b19041e2f9f0d7ba6d3423f200fc
Benny <w3ye@pm.me>
2021-02-04 23:15:21 -0500

    init config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+#! python3
+# config.py - Read and store user configs
+
+class Config:
+
+    # TODO: Check if there is a config filepath
+    def check_path(self)-> bool:
+        pass
+
+    # TODO: Create a config file
+    # TODO: Create a database config file
+    def create_database_config(self):
+        pass
+        # TODO: Store schema name
+    
+    # TODO: Create a local user config 
+    def locall_user_config(self):
+        pass
+        # TODO: Store local username 
+        # TODO: Store local password
+    pass
