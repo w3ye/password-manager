27790f230b0eaa7c595ccbe8320435bf7832194b
Benny <w3ye@pm.me>
2021-02-08 21:43:24 -0500

    added input to add_account method

--- menu.py
+++ menu.py
@@ -2,6 +2,7 @@
 # menu.py - A menu for the user 
 from databaseManager import DatabaseManager as dm
 from query import GenereateQuery
+from genereateId import GenerateId as gId
 import pyinputplus as pyip
 import sys,re
 import password 
@@ -24,18 +25,16 @@ class Menu:
 
         self.main_menu()
         
-
-
     def main_menu(self):
         menuArr = ["Enter an account",
-            "Find an account",
+            "Find an account (If you want to remove an account, find the account first)",
             "Generate a password",
             "QUIT"]
         print('=' * 20 + ' MENU ' + '='*20)
         option = pyip.inputMenu(menuArr, numbered=True, blank=True).lower()
         if option == "enter an account":
             self.account_menu()
-        if option == "quit":
+        if option == menuArr[-1]:
             sys.exit()
     
     def remove_spaces(self, opt):
@@ -45,19 +44,19 @@ class Menu:
     def account_menu(self):
         menuArr = ["Enter an existing account",
             "Creating a new account",
-            "Find an account (If you want to remove an account, find the account first)",
             "Main Menu",
             "QUIT"]
-        print(query)
         print('=' * 20 + ' ACCOUNT ' + '='*20)
         option = pyip.inputMenu(menuArr, numbered=True, blank=True).lower()
         
-        if option == menuArr[-2]:
+        if option == "main menu":
             self.main_menu()
         if option == menuArr[-1]:
             sys.exit()
 
     def add_account(self):
+        accountId = gId().generate_account_id()
+        print(accountId)
         pass
 
     def find_account(self):
@@ -65,5 +64,15 @@ class Menu:
 
     def remove_account(self):
         pass
-    
-Menu()
+
+class Test:
+    def add_account(self):
+        accountId = gId().generate_account_id()
+        username = pyip.inputStr("Enter username")
+        password = pyip.inputStr("Enter the password")
+        appName = pyip.inputStr("Enter the App name")
+        notes = pyip.inputStr("Enter note(Enter to skip)")
+        notes = re.compile(r'*\s*').sub('',notes)
+        
+
+Test().add_account()
