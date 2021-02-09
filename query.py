fbb5830d732720dfd4f299312260736af3e34bb2
Benny <w3ye@pm.me>
2021-02-08 19:21:20 -0500

    constructor added to recieve table name

--- query.py
+++ query.py
@@ -2,6 +2,10 @@
 # query.py - Generate sql queries
 class GenereateQuery:
     
+    def __init__(self, tableName):
+        self.tableName = tableName
+
+    # TODO: Read the user config file to get the table name OR receive the table name from menu
     def new_account(self, accountId: int, username: str, password: str,url: str,note: str) -> str:
         """
         Generate a queries to insert an array of account information to mysql
@@ -9,28 +13,28 @@ class GenereateQuery:
         # Check if note is empty
         k = note.replace(' ','')
         if len(k) != 0:
-            query = "INSERT INTO accounts.acc (account_id, username, psword, url, note) VALUES (%s, '%s', '%s', '%s', '%s')" % (accountId, username, password, url, note)
+            query = "INSERT INTO %s (account_id, username, psword, url, note) VALUES (%s, '%s', '%s', '%s', '%s')" % (self.tableName, accountId, username, password, url, note)
         else: 
-            query = "INSERT INTO accounts.acc (account_id, username, psword, url) VALUES (%s, '%s', '%s', '%s')" % (accountId, username, password, url)
+            query = "INSERT INTO %s (account_id, username, psword, url) VALUES (%s, '%s', '%s', '%s')" % (self.tableName, accountId, username, password, url)
         return query
 
     def find_account(self, choice: str) -> str:
         """
         Generate a queries to find all information to an account
         """
-        query = "SELECT * FROM accounts.acc WHERE %s " % choice 
+        query = "SELECT * FROM %s WHERE %s " % (self.tableName, choice)
         return query
     
     def delete_account(self, accountId: int) -> str:
         """
         Generate a query to delete an account by accountId(primary key)
         """
-        query = "DELETE FROM accounts.acc WHERE account_id = %s" % accountId
+        query = "DELETE FROM %s WHERE account_id = %s" % (self.tableName, countId)
         return query
     
     def update_account(self, accountId: int, newPassword: str) -> str:
         """
         Generate an query to update account password using accountId(primary key)
         """
-        query = "UPDATE accounts.acc SET password = '%s' WHERE account_id = %s" % (accountId, newPassword)
+        query = "UPDATE %s SET password = '%s' WHERE account_id = %s" % (self.tableName, accountId, newPassword)
         return query
